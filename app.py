a940340f445e366644e23d429c668ed7aaa1c387
pt3k <bpieniak1@gmail.com>
2020-10-25 01:40:55 +0200

    add requirements

--- app.py
+++ app.py
@@ -6,10 +6,14 @@ import cv2
 from cv2 import cv2
 import base64
 import string
-
-#os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+import sys
+import logging
 
 app = flask.Flask(__name__, template_folder='templates')
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
 model = keras.models.load_model('./Models/full_model_cnn')
 labels = ["%d" %i for i in range(0,10)] + list(string.ascii_uppercase) #list of strings 0-9 A-Z
 
@@ -36,11 +40,10 @@ def predict():
 	if(vect.sum() == 0):
 		return flask.render_template('draw.html',  prediction = None)
 	
-	print('D')
 	pred = model.predict(vect)
 	index_pred = np.argmax(pred)
 	
 	return flask.render_template('draw.html', prediction = labels[index_pred])
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run()
